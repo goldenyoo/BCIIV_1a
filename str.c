88c1706e72c83b770b1099de8b9be94c41ff80bb
goldenyoo <goldenyoo@kaist.ac.kr>
2019-03-30 22:43:10 +0900

    2019-03-30 22:43

---
+++ str.c
@@ -0,0 +1,162 @@
+#include <assert.h> /* to use assert() */
+#include <stdio.h>
+#include "str.h"
+#define MAX_SIZE 100
+
+/* Your task is: 
+   1. Rewrite the body of "Part 1" functions - remove the current
+      body that simply calls the corresponding C standard library
+      function.
+   2. Write appropriate comment per each function
+*/
+
+/* Part 1 */
+/*------------------------------------------------------------------------*/
+size_t StrGetLength(const char* pcSrc)
+{
+  const char *pcEnd;
+  assert(pcSrc); /* NULL address, 0, and FALSE are identical. */
+  pcEnd = pcSrc;
+	
+  while (*pcEnd) /* null character and FALSE are identical. */
+    pcEnd++;
+
+  return (size_t)(pcEnd - pcSrc);
+}
+
+/*------------------------------------------------------------------------*/
+char *StrCopy(char *pcDest, const char* pcSrc)
+{
+  /* TODO: fill this function */
+	char *pcAsg;
+	const char *pcCpy;
+
+	//assert(pcEnd);
+	pcAsg = pcDest;
+	pcCpy = pcSrc;
+	
+	if(*pcCpy == '\0'){
+		*pcAsg = '\0';
+		return pcDest;
+	}
+	while(*pcCpy != '\0'){
+		*pcAsg = *pcCpy;
+		pcAsg++;
+		pcCpy++;
+	}
+  	*pcAsg = '\0';
+  return pcDest;
+}
+
+/*------------------------------------------------------------------------*/
+int StrCompare(const char* pcS1, const char* pcS2)
+{
+  /* TODO: fill this function */
+	const char* pcOne;
+	const char* pcSec;
+	
+	pcOne = pcS1;
+	pcSec = pcS2;
+
+	while(*pcOne!='\0' && *pcSec!='\0'){
+		if(*pcOne == *pcSec ){
+			pcOne++;
+			pcSec++;
+		}
+		else{
+			return *pcOne - *pcSec;
+		}
+	}
+ 
+  return *pcOne - *pcSec;
+}
+
+/*------------------------------------------------------------------------*/
+char *StrSearch(const char* pcHaystack, const char *pcNeedle)
+{
+  /* TODO: fill this function */
+	//assert(*pcNeedle);
+
+	enum DFAState {FIND, NON};
+	enum DFAState state;
+
+	state = NON;
+
+	const char* pcOrig;
+	//const char* pcTarget;
+	const char* pcFind;
+	const char* pcTmp;
+
+	pcOrig = pcHaystack;
+	//pcTarget = pcNeedle;
+	pcTmp = pcNeedle;
+	pcFind = NULL;
+	if(!(*pcNeedle)){
+		return (char*)	pcHaystack;
+	}
+	while(*pcOrig){
+		switch(state){
+			case NON:
+				if(*pcOrig == *pcNeedle){
+					pcFind = pcOrig;
+					pcOrig++;
+					pcTmp++;
+					state = FIND;
+				}
+				else{
+					pcOrig++;
+					state = NON;
+				}
+				break;
+
+			case FIND:
+				
+				if(*pcOrig == *pcTmp){
+					pcOrig++;
+					pcTmp++;
+					state = FIND;
+					if(*pcTmp == '\0'){
+						return (char*)pcFind;
+					}
+				}
+				else{
+					pcTmp = pcNeedle;
+					state = NON;
+				}
+				break;
+
+			default:
+				assert(0);
+				break;
+		}
+	}
+	return (char*) NULL; 
+}
+/*------------------------------------------------------------------------*/
+char *StrConcat(char *pcDest, const char* pcSrc)
+{
+  /* TODO: fill this function */
+	//char c_add[MAX_SIZE];
+	char *c_first;
+	const char *c_second;
+	char *c_output;
+
+	c_first = pcDest;
+	c_second = pcSrc;
+
+	c_output = c_first;
+	//c_output = StrCopy(c_add,c_first);
+
+	while(*c_output != '\0'){
+		c_output++;
+	}
+
+	while(*c_second != '\0'){
+		*c_output = *c_second;
+		c_output++;
+		c_second++;
+	}
+	*c_output = '\0';
+
+  return pcDest;	
+}
---
+++ test.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h> 
+
+size_t StrGetLength(const char* pcSrc)
+{
+  const char *pcEnd;
+  assert(pcSrc); /* NULL address, 0, and FALSE are identical. */
+  pcEnd = pcSrc;
+	
+  while (*pcEnd) /* null character and FALSE are identical. */
+    pcEnd++;
+
+  return (size_t)(pcEnd - pcSrc);
+}
+
+
+char *StrCopy(char *pcDest, const char* pcSrc)
+{
+  /* TODO: fill this function */
+	int i;
+	char *pcStart;
+	const char *pcEnd;
+	//assert(pcEnd);
+	pcStart = pcDest;
+
+	for(i = 0; i < StrGetLength(pcSrc); i++){
+		*pcStart = *pcSrc;
+		pcStart++;
+		pcSrc++;
+	}
+  
+  return pcDest;
+}
+int StrCompare(const char* pcS1, const char* pcS2)
+{
+  /* TODO: fill this function */
+	const char* pcOne;
+	const char* pcSec;
+	pcOne = pcS1;
+	pcSec = pcS2;
+
+	while(*pcOne && *pcSec){
+		if(*pcOne == *pcSec ){
+			pcOne++;
+			pcSec++;
+		}
+		else{
+			return *pcOne - *pcSec;
+		}
+	}
+ 
+  return *pcOne - *pcSec;
+}
+
+char *StrSearch(const char* pcHaystack, const char *pcNeedle)
+{
+  /* TODO: fill this function */
+	enum DFAState {FIND, NON};
+	enum DFAState state;
+
+	state = NON;
+
+	const char* pcOrig;
+	const char* pcTarget;
+	const char* pcFind;
+	const char* pcTmp;
+
+	pcOrig = pcHaystack;
+	pcTarget = pcNeedle;
+	pcTmp = pcNeedle;
+	pcFind = NULL;
+
+	while(*pcOrig){
+		switch(state){
+			case NON:
+				if(*pcOrig == *pcNeedle){
+					pcFind = pcOrig;
+					pcOrig++;
+					pcTmp++;
+					state = FIND;
+				}
+				else{
+					pcOrig++;
+					state = NON;
+				}
+				break;
+
+			case FIND:
+				
+				if(*pcOrig == *pcTmp){
+					pcOrig++;
+					pcTmp++;
+					state = FIND;
+					if(*pcTmp == '\0'){
+						return (char*)pcFind;
+					}
+				}
+				else{
+					pcTmp = pcNeedle;
+					state = NON;
+				}
+				break;
+
+			default:
+				assert(0);
+				break;
+		}
+	}
+	return (char*) NULL; 
+}
+
+int main(void){
+	const char *s1 = "aaaaa"; // 9797102
+	char s2[128];
+	const char *s3 = "baaaa";// 100
+	char str_empty[] = "";
+	char *res2;
+  char str2[] = "StrSearch with Null\0 Following character "
+    "should not be searched";
+	res2 = strstr(str2, str_empty);
+printf("String.h Answer: [%s]\n", res2);
+	const char *s4 = "a acbboy at ";// 100
+	const char *s5 = "boy";// 100
+	const char *ptr = "\0";
+
+
+	printf("%s\n", ptr);
+	// ptr = strstr(s4,s5);
+	// printf("%s\n", ptr);
+	// ptr = StrSearch(s4,s5);
+	// printf("%s\n", ptr);
+
+/* strcpy()*/
+	// strcpy(s2,s1);
+	// printf("%s\n", s2);
+
+	// StrCopy(s2,s1);
+	// printf("%s\n", s2);
+	// int len;
+	// len = strlen(s1);
+	// printf("%d\n", len);
+
+	// printf("%d \n", strcmp(s1,s3));
+	// printf("%d \n", StrCompare(s1,s3));
+	
+
+	
+	return 0;
+}
