3ecf034d395028416781b1ecbde753253bb9012a
goldenyoo <goldenyoo@kaist.ac.kr>
2019-05-14 20:41:40 +0900

    20:41

--- mydc.s
+++ mydc.s
@@ -111,20 +111,72 @@ input:
 	movl $q, %eax
 	movb (%eax), %cl
 	cmp %ecx, %ebx
-	cmp %ecx, %ebx
 	je quit
 
+	# buffer[0] == '+'
+	movl $0, %ecx
+	movl $plus, %eax
+	movb (%eax), %cl
+	cmp %ecx, %ebx
+	je is_plus
 
-	# movl $buffer, %eax
-	# pushl %eax
-	# call atoi
-	# addl $4, %esp
+	# buffer[0] == '-'
+	movl $0, %ecx
+	movl $minus, %eax
+	movb (%eax), %cl
+	cmp %ecx, %ebx
+	je is_minus
 
-	# pushl %eax
+	# buffer[0] == '*'
+	movl $0, %ecx
+	movl $multiply, %eax
+	movb (%eax), %cl
+	cmp %ecx, %ebx
+	je is_multiply
 
-	# pushl $printfFormat
-	# call printf
-	# addl $8, %esp
+	# buffer[0] == '/'
+	movl $0, %ecx
+	movl $division, %eax
+	movb (%eax), %cl
+	cmp %ecx, %ebx
+	je is_division
+
+	# buffer[0] == '%'
+	movl $0, %ecx
+	movl $modulus, %eax
+	movb (%eax), %cl
+	cmp %ecx, %ebx
+	je is_modulus
+
+	# buffer[0] == 'f'
+	movl $0, %ecx
+	movl $f, %eax
+	movb (%eax), %cl
+	cmp %ecx, %ebx
+	movl %esp, %ecx
+	je is_f_before
+
+	# buffer[0] == 'c'
+	movl $0, %ecx
+	movl $c, %eax
+	movb (%eax), %cl
+	cmp %ecx, %ebx
+	je is_c
+
+	# buffer[0] == 'd'
+	movl $0, %ecx
+	movl $d, %eax
+	movb (%eax), %cl
+	cmp %ecx, %ebx
+	je is_d
+
+	# buffer[0] == 'r'
+	movl $0, %ecx
+	movl $r, %eax
+	movb (%eax), %cl
+	cmp %ecx, %ebx
+	je is_r
+	
 	jmp quit
 
 
@@ -146,8 +198,131 @@ is_p:
 	# addl $8, %esp
 	# jmp quit
 
+is_plus:
+	cmpl %esp, %ebp
+	je is_empty
+
+	movl (%esp), %eax
+	addl $4, %esp
+
+	cmpl %esp, %ebp
+	je is_incomplete
+
+	movl (%esp), %ebx
+	addl $4, %esp
+
+	addl %ebx, %eax
+	pushl %eax
+
+	jmp input
+
+is_minus:
+	cmpl %esp, %ebp
+	je is_empty
+
+	movl (%esp), %eax
+	addl $4, %esp
+
+	cmpl %esp, %ebp
+	je is_incomplete
+
+	movl (%esp), %ebx
+	addl $4, %esp
+
+	subl %eax, %ebx
+	pushl %ebx
+
+	jmp input
+
+is_multiply:
+	cmpl %esp, %ebp
+	je is_empty
+
+	movl (%esp), %eax
+	addl $4, %esp
+
+	cmpl %esp, %ebp
+	je is_incomplete
+
+	movl (%esp), %ebx
+	addl $4, %esp
+
+	imul %ebx
+
+	pushl %eax
+
+	jmp input
+
+is_division:
+	cmpl %esp, %ebp
+	je is_empty
+
+	movl (%esp), %ebx
+	addl $4, %esp
+
+	cmpl %esp, %ebp
+	je is_incomplete
+
+	movl (%esp), %eax
+	CDQ
+	addl $4, %esp
 
 
+	idiv %ebx
+
+	pushl %eax
+
+	jmp input
+
+is_modulus:
+	cmpl %esp, %ebp
+	je is_empty
+
+	movl (%esp), %ebx
+	addl $4, %esp
+
+	cmpl %esp, %ebp
+	je is_incomplete
+
+	movl (%esp), %eax
+	CDQ
+	addl $4, %esp
+
+	idiv %ebx
+
+	pushl %edx
+
+	jmp input
+
+is_f_before:
+	cmpl %esp, %ebp
+	je is_empty
+
+	jmp is_f
+
+is_f:
+	cmpl  %ecx, %ebp
+	jle input
+
+	pushl %ecx
+	pushl (%ecx)
+	pushl $printfFormat
+	call printf
+	addl $8, %esp
+	popl %ecx
+
+	addl $4, %ecx
+	jmp is_f
+
+is_c:
+	movl    %ebp, %esp
+	jmp input
+
+is_d:
+	jmp input
+
+is_r:
+	jmp input
 
 is_digit:
 	movl $buffer,%ebx
@@ -157,14 +332,16 @@ is_digit:
 
 	pushl %eax
 
-	# pushl (%esp)
-	# pushl $printfFormat
-	# call printf
-	# addl $8, %esp
 	jmp input
 
 
-	
+is_incomplete:
+	pushl %eax
+	pushl $emptyErrorMSg
+	call printf
+	addl $4, %esp
+	jmp input
+
 is_empty:
 	pushl $emptyErrorMSg
 	call printf
