4adc03445f85f3f52262f9dcb89d7c80851e7739
goldenyoo <goldenyoo@kaist.ac.kr>
2021-01-14 20:48:32 +0900

    .

--- deep_learning_cnn.m
+++ deep_learning_cnn.m
@@ -2,7 +2,7 @@
 %    File_name: deep_learning_cnn.m
 %    Programmer: Seungjae Yoo                             
 %                                           
-%    Last Modified: 2020_01_13                            
+%    Last Modified: 2020_01_14                           
 %                                                            
  % ----------------------------------------------------------------------- %
  %% Call raw data
@@ -35,6 +35,28 @@ cnt = cnt';
 
 % Only include MI related electrode 
 cnt_c = [cnt(10:16,:); cnt(26:32,:); cnt(42:48,:)];
+
+% Z-score normalization
+m = mean(cnt_c,2);
+sigma = std(cnt_c,0,2);
+for i = 1: size(cnt_c,1)
+    cnt_c(i,:) = cnt_c(i,:) - m(i)*ones(1,size(cnt_c,2));
+    cnt_c(i,:) = cnt_c(i,:)./sigma(i);
+end
+
+
+% %% Re-referening
+% % difference to right ear electrode
+% for i = 1 : length(cnt_c)
+%     cnt_c(:,i) = cnt_c(:,i) - cnt(33,i) * ones(size(cnt_c,1),1);
+% end
+% 
+% % common average
+% Means = mean(cnt_c);
+% for i = 1: length(Means)
+%     cnt_c(:,i) = cnt_c(:,i) - Means(1,i)*ones(size(cnt_c,1),1);
+% end
+
 %% 
 %train test split
 a = 1; b = 1;
@@ -73,7 +95,7 @@ YTest = categorical(YTest);
 
 %%
 inputSize = 21;
-numHiddenUnits = 50; %%%%%%%%%%%%%% Change hidden unit number
+numHiddenUnits = 30; %%%%%%%%%%%%%% Change hidden unit number
 numClasses = 2;
 
 layers = [ ...
@@ -84,7 +106,7 @@ layers = [ ...
     softmaxLayer
     classificationLayer]; % Can Add: InputWeightsLearnRateFactor, RecurrentWeightsLearnRateFactor, BiasLearnRateFactor, InputWeightsL2Factor
 
-maxEpochs = 100;
+maxEpochs = 300;
 miniBatchSize = 351; %%%%%%%%%%%%%% Change BatchSize -> our data is already chunked
 
 options = trainingOptions('adam', ...
