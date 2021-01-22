f4ebf05d8acec1cbf12ea661448d174919bf8ab0
goldenyoo <goldenyoo@kaist.ac.kr>
2021-01-22 21:00:19 +0900

    using both calib, eval data / modify BPF

--- deep_learning_cnn.m
+++ deep_learning_cnn.m
@@ -2,18 +2,18 @@
 %    File_name: deep_learning_cnn.m
 %    Programmer: Seungjae Yoo                             
 %                                           
-%    Last Modified: 2020_01_14                           
+%    Last Modified: 2020_01_22                           
 %                                                            
  % ----------------------------------------------------------------------- %
- %% Call raw data
+ %% Call raw Training data
 close all
 clear all
 
 % Ask user for input parameters
-prompt = {'Data label: ','train test split ratio: '};
+prompt = {'Data label: ', 'Feature vector length: ', 'Re-referencing: 0 (Non),1 (CAR), 2 (LAP)', 'BFP order'};
 dlgtitle = 'Input';
 dims = [1 50];
-definput = {'a','0.8'};
+definput = {'a', '3', '1','20'};
 answer = inputdlg(prompt,dlgtitle,dims,definput);
 
 
@@ -22,8 +22,9 @@ if isempty(answer), error("Not enough input parameters."); end
 
 % Input parameters
 data_label = string(answer(1,1));   % Calib_ds1 + "data_label"
-train_test_ratio = double(string(answer(2,1))); % train_test_split ratio
-
+m = double(string(answer(2,1))); % feature vector will have length (2m)
+referencing = double(string(answer(3,1)));
+order = double(string(answer(4,1)));
 
 % Load file
 FILENAME = strcat('C:\Users\유승재\Desktop\Motor Imagery EEG data\BCICIV_1_mat\BCICIV_calib_ds1',data_label,'.mat');
@@ -33,66 +34,140 @@ load(FILENAME);
 cnt= 0.1*double(cnt);
 cnt = cnt';
 
-% Only include MI related electrode 
-cnt_c = [cnt(10:16,:); cnt(26:32,:); cnt(42:48,:)];
-% cnt_c = [cnt(26,:);cnt(27,:); cnt(31,:); cnt(32,:)];
+% Exclude electrode (AF3, AF4, O1, O2, PO1, PO2)
+cnt_c = cnt(3:55,:);
 
+%% Preprocessing
+if referencing ~= 0
+    %%% Calculate differential voltage
+    for i = 1 : size(cnt_c,1)
+        cnt_c(i,:) = cnt_c(i,:) - cnt(29,:);
+    end
 
+    
+    if referencing == 1 % common average
+        Means = (1/size(cnt_c,1))*sum(cnt_c);
+        for i = 1 : size(cnt_c,1)
+            cnt_c(i,:) = cnt_c(i,:) - Means;
+        end
+    elseif referencing == 2 % LAP
+        %%%
+    end
+end
+%% BPF
+%BPF Design
+bpFilt = designfilt('bandpassfir','FilterOrder',order, ...
+         'CutoffFrequency1',8,'CutoffFrequency2',30, ...
+         'SampleRate',100);
 
-% %% Re-referening
-% % difference to right ear electrode
-% for i = 1 : length(cnt_c)
-%     cnt_c(:,i) = cnt_c(:,i) - cnt(33,i) * ones(size(cnt_c,1),1);
-% end
-% 
-% % common average
-% Means = mean(cnt_c);
-% for i = 1: length(Means)
-%     cnt_c(:,i) = cnt_c(:,i) - Means(1,i)*ones(size(cnt_c,1),1);
-% end
-% 
-% % Z-score normalization
-% m = mean(cnt_c,2);
-% sigma = std(cnt_c,0,2);
-% for i = 1: size(cnt_c,1)
-%     cnt_c(i,:) = cnt_c(i,:) - m(i)*ones(1,size(cnt_c,2));
-%     cnt_c(i,:) = cnt_c(i,:)./sigma(i);
-% end
+% Apply BPF
+for i = 1:size(cnt_c,1)
+    cnt_c(i,:) = filtfilt(bpFilt, cnt_c(i,:));
+end
 
+%% Training set
 
-%% 
-%train test split
-a = 1; b = 1;
-% s = rng;
-r = randperm(length(mrk.pos));
-k = round(length(r)*train_test_ratio);
-
-for j = 1:k
-    i = r(1,j);
+for i = 1:length(mrk.pos)
     
     % One trial data
     E = cnt_c(:,mrk.pos(1,i):mrk.pos(1,i)+350);   
     
-    XTrain(j,1) = {E};    
-    YTrain(j,1) = mrk.y(1,i);
-    if mrk.y(1,i) == 1 
-       
-        a = a+1;
-    else
-       
-        b = b+1;
-    end
+    XTrain(i,1) = {E};    
+    YTrain(i,1) = mrk.y(1,i);
+    
 end
 YTrain = categorical(YTrain);
 
-for j = k+1:length(r)
-    i = r(1,j);
+%% True Label
+
+FILENAME = strcat('C:\Users\유승재\Desktop\true_labels\BCICIV_eval_ds1',data_label,'_1000Hz_true_y.mat');
+load(FILENAME);
+A=[];
+B=[];
+C=[];
+D=[];
+%NaN, -1, 0, 1
+tmp = 1;
+for i=1:length(true_y)-1
+    if isnan(true_y(i,1))
+        if isnan(true_y(i+1,1))
+            continue
+        else
+            A = [A; [tmp i]];
+            tmp = i+1;
+        end
+    elseif true_y(i,1) ~= true_y(i+1,1)
+        if true_y(i,1)== -1
+            B = [B; [tmp i]];
+        elseif true_y(i,1)== 0
+            C = [C; [tmp i]];
+        elseif true_y(i,1)== 1
+            D = [D; [tmp i]];
+        end
+        tmp = i+1;
+    end
+end
+
+
+%% Call raw Test data
+% Load file
+FILENAME = strcat('C:\Users\유승재\Desktop\Motor Imagery EEG data\BCICIV_1_mat\BCICIV_eval_ds1',data_label,'.mat');
+load(FILENAME);
+
+% Data rescale
+cnt= 0.1*double(cnt);
+cnt = cnt';
+
+% Exclude electrode (AF3, AF4, O1, O2, PO1, PO2)
+cnt_c = cnt(3:55,:);
+
+%% Preprocessing
+if referencing ~= 0
+    %%% Calculate differential voltage
+    for i = 1 : size(cnt_c,1)
+        cnt_c(i,:) = cnt_c(i,:) - cnt(29,:);
+    end
+
     
-    % One trial data
-    E = cnt_c(:,mrk.pos(1,i):mrk.pos(1,i)+350);   
+    if referencing == 1 % common average
+        Means = (1/size(cnt_c,1))*sum(cnt_c);
+        for i = 1 : size(cnt_c,1)
+            cnt_c(i,:) = cnt_c(i,:) - Means;
+        end
+    elseif referencing == 2 % LAP
+        %%%
+    end
+end
+
+%% BPF
+%BPF Design
+bpFilt = designfilt('bandpassfir','FilterOrder',order, ...
+         'CutoffFrequency1',8,'CutoffFrequency2',30, ...
+         'SampleRate',100);
+
+% Apply BPF
+for i = 1:size(cnt_c,1)
+    cnt_c(i,:) = filtfilt(bpFilt, cnt_c(i,:));
+end
+%% Test set
+for j = 1 : length(B)
+    tmp1 = round(B(j,1)/10);
+    tmp2 = round(B(j,2)/10);
+    E = cnt_c(:, tmp1:tmp2);
     
-    XTest(j-k,1) = {E};    
-    YTest(j-k,1) = mrk.y(1,i);
+    XTest(j,1) = {E};    
+    YTest(j,1) = -1;
+ 
+end
+
+for j = 1 : length(D)
+    tmp1 = round(D(j,1)/10);
+    tmp2 = round(D(j,2)/10);
+    E = cnt_c(:, tmp1:tmp2);
+    
+    XTest(length(B)+j,1) = {E};    
+    YTest(length(B)+j,1) = 1;
+ 
 end
 YTest = categorical(YTest);
 
@@ -110,13 +185,12 @@ layers = [ ...
     classificationLayer]; % Can Add: InputWeightsLearnRateFactor, RecurrentWeightsLearnRateFactor, BiasLearnRateFactor, InputWeightsL2Factor
 
 maxEpochs = 300;
-miniBatchSize = 351; %%%%%%%%%%%%%% Change BatchSize -> our data is already chunked
+% miniBatchSize = 351; %%%%%%%%%%%%%% Change BatchSize -> our data is already chunked
 
 options = trainingOptions('adam', ...
     'ExecutionEnvironment','auto', ...
     'GradientThreshold',1, ...
     'MaxEpochs',maxEpochs, ...
-    'MiniBatchSize',miniBatchSize, ...
     'SequenceLength','longest', ...
     'Shuffle','never', ...   % "once', 'never', 'every-epoch'
     'ValidationData',{XTest,YTest}, ...
